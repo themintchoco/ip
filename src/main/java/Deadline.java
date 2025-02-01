9c7934a28577204a69c3f6dd7a43df7967f5f4dc
mint <76428902+themintchoco@users.noreply.github.com>
2025-02-02 00:39:39 +0800

    feat: Add datetime parsing

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,22 @@
-public class Deadline extends Task {
-    private String endTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String endTime) {
+public class Deadline extends Task {
+    private LocalDateTime endTime;
+    private static DateTimeFormatter dtfParse = DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm");
+    private static DateTimeFormatter dtfDisplay = DateTimeFormatter.ofPattern("MMM dd yyyy HHmm");
+
+    public Deadline(String description, String endTimeString) {
         super(description);
-        this.endTime = endTime;
+        this.endTime = LocalDateTime.parse(endTimeString, dtfParse);
     }
 
     public String toSaveString() {
-        return String.format("D | %s | %s", super.toSaveString(), endTime);
+        return String.format("D | %s | %s", super.toSaveString(), endTime.format(dtfParse));
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), endTime);
+        return String.format("[D]%s (by: %s)", super.toString(), endTime.format(dtfDisplay));
     }
 }
